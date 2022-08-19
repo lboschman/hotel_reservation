bc70f69a859e506440943368cf60a86f41d8976b
Leon Boschman <leon.boschman@gmail.com>
2022-08-19 14:27:56 +0200

    Added room overview endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
-from email import message
 from fastapi import FastAPI
+from sqlalchemy import select
+
+from database.make_engine import make_engine, make_engine
+from database.setup_tables import Room, RoomCategory, Reservation
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/room")
+async def room():
+    stmt = select([
+        Room.room_number,
+        Room.category
+    ])
+    engine = make_engine()
+    conn = engine.connect()
+    results = conn.execute(stmt).fetchall()
+    return results
+
